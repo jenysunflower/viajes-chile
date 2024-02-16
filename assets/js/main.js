b9b4c833eadc13f1ed782672def10989747e278a
jenysunflower <cosasdejeny@gmail.com>
2024-02-16 16:50:46 -0300

    Se agrega función para cambiar de color el menú según movimientos

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,31 @@ $(function(){
                 alert("El correo fue enviado correctamente.")
             });
 
+  
+    /* Función para cambiar los colores según se mueve y según se abre */
+    $(document).ready(function() {
+        var navbar = $('.navbar');
+        var navbarToggler = $('.navbar-toggler');
+      
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 50 || navbarToggler.attr('aria-expanded') === 'true') {
+            navbar.addClass('navbar-scrolled');  /* Si el usuario se mueve más de 50 px se activa la clase que crees en el css, en este caso "navbar-scrolled" sino se desactiva  */
+          } else {
+            navbar.removeClass('navbar-scrolled');
+          }
+        });
+      
+        navbarToggler.on('click', function() {
+          if (navbarToggler.attr('aria-expanded') === 'true') { /* Si el usuario abre el navbar también se activa el color para que no quede transparente */
+            navbar.addClass('navbar-scrolled');
+          } else {
+            
+          }
+        });
+      });
+      
+
+
 
 
 
