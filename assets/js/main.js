7b6a83a871a1ac1113f6148a4f0988e0a269c686
jenysunflower <cosasdejeny@gmail.com>
2024-02-16 21:27:59 -0300

    últimos detalles, proyecto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,57 @@
-$(function(){
+$(function () {
 
-    /* Se agrega para tooltips */
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  /* Se agrega para tooltips */
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    
 
- /*    var correoClick = $('#enviarEmail');
-            correoClick.click(function(e) {
-                alert("El correo fue enviado correctamente.")
-            });
- */
-  
-    /* Función para cambiar los colores según se mueve y según se abre */
-    $(document).ready(function() {
-        var navbar = $('.navbar');
-        var navbarToggler = $('.navbar-toggler');
-      
-        $(window).scroll(function() {
-          if ($(this).scrollTop() > 50 || navbarToggler.attr('aria-expanded') === 'true') {
-            navbar.addClass('navbar-scrolled');  /* Si el usuario se mueve más de 50 px se activa la clase que crees en el css, en este caso "navbar-scrolled" sino se desactiva  */
-          } else {
-            navbar.removeClass('navbar-scrolled');
-          }
+
+  /*    var correoClick = $('#enviarEmail');
+             correoClick.click(function(e) {
+                 alert("El correo fue enviado correctamente.")
+             });
+  */
+
+  /* Función para cambiar los colores según se mueve y según se abre */
+  $(document).ready(function () {
+    var navbar = $('.navbar');
+    var navbarToggler = $('.navbar-toggler');
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 50 || navbarToggler.attr('aria-expanded') === 'true') {
+        navbar.addClass('navbar-scrolled');  /* Si el usuario se mueve más de 50 px se activa la clase que crees en el css, en este caso "navbar-scrolled" sino se desactiva  */
+      } else {
+        navbar.removeClass('navbar-scrolled');
+      }
+    });
+
+    navbarToggler.on('click', function () {
+      if (navbarToggler.attr('aria-expanded') === 'true') { /* Si el usuario abre el navbar también se activa el color para que no quede transparente */
+        navbar.addClass('navbar-scrolled');
+      } else {
+
+      }
+    });
+  });
+
+
+
+  /* Función para scroll */
+  $(document).ready(function () {
+    $("a").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+
+        // Función de Jquery
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
         });
-      
-        navbarToggler.on('click', function() {
-          if (navbarToggler.attr('aria-expanded') === 'true') { /* Si el usuario abre el navbar también se activa el color para que no quede transparente */
-            navbar.addClass('navbar-scrolled');
-          } else {
-            
-          }
-        });
-      });
-      
-
-      
-      /* Función para scroll */
-      $(document).ready(function(){
-        $("a").on('click', function(event) {
-          if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-  
-            // Función de Jquery
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top
-            }, 800, function(){
-              window.location.hash = hash;
-            });
-          } 
-        });
-      });
+      }
+    });
+  });
 
 
 });
